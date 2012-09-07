1d552672faa70e2fd5f2e39a966fd7cf2c7aeac7
nicodeceulaer <nico.de.ceulaer@gmail.com>
2012-09-07 19:57:09 +0300

    Update lib/kaminari/models/active_record_relation_methods.rb
    take the proposal at the end of the discussion on https://github.com/amatsuda/kaminari/pull/160

--- lib/kaminari/models/active_record_relation_methods.rb
+++ lib/kaminari/models/active_record_relation_methods.rb
@@ -19,7 +19,7 @@ module Kaminari
         # a workaround to count the actual model instances on distinct query because count + distinct returns wrong value in some cases. see https://github.com/amatsuda/kaminari/pull/160
         uses_distinct_sql_statement = c.to_sql =~ /DISTINCT/i
         if uses_distinct_sql_statement
-          c.length
+          ActiveRecord::Base.connection.select_values(c.to_sql).length
         else
           # .group returns an OrderdHash that responds to #count
           c = c.count
